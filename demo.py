7754d8487c01680b28368b3179709f69e4804239
Robert <roberkit2000@gmail.com>
2026-08-15 08:02:40 +0300

    changed line 55

--- demo.py
+++ demo.py
@@ -17,13 +17,13 @@ class Book:
 class Library:
     def __init__(self):
         self.books={} # isbn -> Book
-    case "1":      
+        
     def add_book(self,book):  # add books to a dictionary stated above
         self.book=book
         self.books[book.isbn]=book  #intialize a key with ISBN into a dcictionary  
         print(f"Added a book:{book.title},{book.author},{book.isbn}, to a library")
 
-    case "2": 
+    
     def checkout_book(self,isbn):
         self.isbn=isbn
         if isbn in self.books:
@@ -35,7 +35,7 @@ class Library:
             else:
                 print("book not available, or has been borrowed")
                     
-     case "3":     
+        
     def return_book(self,isbn):
         self.isbn=isbn
         for book in self.books:
@@ -46,15 +46,15 @@ class Library:
                     return True
                 else:
                     print("book not available, or has been borrowed")
-    case "4":         
+            
     def list_books(self):
         if not self.books: ## checks if a list is empty
             print("No books in library")
             return
 
-            for book in self.books:   ## loop for print list values
+            for book in self.books.values():   ## loop for print list values
                 print(book)
-    case "5":         
+           
     def find_by_author(self, author):
             for book in self.books:
                 if book.author==author:
@@ -69,59 +69,24 @@ book3=Book("E-commerce","Ken morris","102")
 book4=Book("Accounting","yunis musa","103")
 
 ## creating objects from Library class
-library=Library()
-library.add_book(book1)
-library.add_book(book2)
-library.add_book(book3)
-library.add_book(book4)
-library.add_book(Book("Java","ken","105")) ## adding a book manually
+lib=Library()
+lib.add_book(book1)
+lib.add_book(book2)
+lib.add_book(book3)
+lib.add_book(book4)
 
 
-## list library Books function
-library.list_books()
+lib = Library()
+lib.add_book(Book("Dune", "Frank Herbert", "111"))
+lib.add_book(Book("Foundation", "Isaac Asimov", "222"))
 
-#check-out books from the library
-library.checkout_book("103")
-library.checkout_book("101")
-library.checkout_book("102")
-library.checkout_book("102")
+lib.checkout_book("111")
+lib.checkout_book("111") # should fail: already checked out
+lib.checkout_book("999") # should fail: no such ISBN
 
-#return books
-library.return_book("103") 
- # calling function to list all books in the library         
-library.list_books()
+lib.list_available_books() # should only show Foundation
 
-#additional menus function
-    def menu(self):
-        print("Welcome to Bungoma High School Library Management System")
-        print("1. Add a book")
-        print("2. Checkout a book")
-        print("3. Return a book")
-        print("4. List all books")
-        print("5. Find books by author")
-        print("6. Exit")
+lib.return_book("111")
 
-option=input("Enter your case choices  (1-5):")  
-if case=="1":
-    library.add_book()
-
-elif case=="2":
-    library.checkout_book()
-
-elif case=="3":
-    library.return_book()
-
-elif case=="4":
-    library.list_books()
-
-elif case=="5":
-    library.find_by_author()
-
-else:
-    print("Invalid option. Please try again.")
-
-
-
-
-
-
+lib.list_available_books() # should show both again
+   
