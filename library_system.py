eb56cb489048ba79086556e140271124980f996f
Robert <roberkit2000@gmail.com>
2026-09-05 13:08:54 +0300

    saved

--- library_system.py
+++ library_system.py
@@ -1,4 +1,5 @@
 class Book:
+    print("-------------Nairobi School Library system-------------")
     def __init__(self, title, author, isbn):
         self.title = title
         self.author = author
@@ -15,22 +16,31 @@ class Book:
         
 class Library:
     def __init__(self):
-        self.books=[] # isbn -> Book
-              
+        self.books={} # isbn -> Book
+        
     def add_book(self,book):  # add books to a dictionary stated above
+        """Add a book to the library."""
         self.book=book
-        self.books.append(book)  #intialize a key with ISBN in an empty list  
+        self.books[book.isbn]=book  #intialize a key with ISBN into a dcictionary  
         print(f"Added a book:{book.title},{book.author},{book.isbn}, to a library")
- 
+
+    
     def checkout_book(self,isbn):
+        print("----------------- Library checkout Catalog---------------------")
         self.isbn=isbn
-        for book in self.books:
-            if book.isbn==isbn and not book.is_checkout_book:
-                book.is_checkout_book==True 
-                print("you have borrowed",{book.title},{book.author},{book.isbn})
+        if isbn in self.books:
+            book = self.books[isbn]
+            if not book.is_checked_out:
+                book.is_checked_out=True 
+                print(f"you have borrowed {book.title} by {book.author} (ISBN: {book.isbn})")
                 return True
+            
+            else:
                 print("book not available, or has been borrowed")
-                    
+
+            if isbn not in self.books:
+                print("isbn not found in library")
+                return      
         
     def return_book(self,isbn):
         self.isbn=isbn
@@ -42,48 +52,47 @@ class Library:
                     return True
                     print("book not available, or has been borrowed")
 
-    def list_books(self):
+               
+    def find_by_author(self, author):
+        for book in self.books:
+            if book.author==author:
+                print(book)
+                return
+                
+    def list_available_books(self):
         if not self.books: ## checks if a list is empty
             print("No books in library")
             return
-            
-        for book in self.books:   ## loop for print list values
+        print("----------------- Library books Catalog---------------------")
+   
+        for book in self.books.values():   ## loop for print list values
             print(book)
-            
-    #def find_by_author(self, author):
-            
+           
 ### create books from Book class       
-book1=Book("software engineering","Charles megon","10000")
-book2=Book("calculus","Grigorii jeff","10001")
-book3=Book("E-commerce","Ken morris","10002")
-book4=Book("Accounting","yunis musa","10003")
+book1=Book("software engineering","Charles megon","100")
+book2=Book("calculus","Grigorii jeff","101")
+book3=Book("E-commerce","Ken morris","102")
+book4=Book("Accounting","yunis musa","103")
 
 ## creating objects from Library class
 library=Library()
 library.add_book(book1)
 library.add_book(book2)
-library.add_book(book3)
-library.add_book(book4)
-library.add_book(Book("Java","ken","10005")) ## adding a book manually
 
+## adding a book manually to a library list
+library.add_book(Book("Java","ken","10005")) 
+library.add_book(Book("Dune", "Frank Herbert", "111"))
+library.add_book(Book("Foundation", "Isaac Asimov", "222"))
 
-## list library Books function
-library.list_books()
+# checking out/ borrowing books from Library
+library.checkout_book("111")
+library.checkout_book("111") # should fail: already checked out
+library.checkout_book("999") # should fail: no such ISBN
 
-library.checkout_book("10003")
-library.checkout_book("10001")
+library.list_available_books() # should only show Foundation
 
+library.return_book("111")
 
-
-#return books
-return_book("10003") #changed return isdn
-    
-         
-#def find_by_author(self, author):
-# TODO
-#    pass
-
-#    def list_available_books(self):
-list_books()# calling function to list all books in the library
-
+library.list_available_books() # should show both again
+library.find_by_author(101)
 
